5d3f8e6efda1b6683d174e9939e7a3c5c4618136
Rolando Angelini <rjar2020@gmail.com>
2024-03-24 15:11:16 +0100

    Initial scaffolding for Superpayment

---
+++ src/main/java/com/superfintech/superpayment/SuperpaymentApplication.java
@@ -0,0 +1,13 @@
+package com.superfintech.superpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperpaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/superfintech/superpayment/SuperpaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superfintech.superpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperpaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
