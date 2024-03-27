db9697a1bc5fb8ae2e56462ffa2fcedce7d32ec0
Rolando Angelini <rjar2020@gmail.com>
2024-03-27 10:38:47 +0100

    Enabling pagination for Payments to enhance the UX and performance at Backoffice level

--- src/main/java/com/superfintech/superpayment/controller/BackofficeController.java
+++ src/main/java/com/superfintech/superpayment/controller/BackofficeController.java
@@ -3,24 +3,27 @@ package com.superfintech.superpayment.controller;
 import com.superfintech.superpayment.entity.Payment;
 import com.superfintech.superpayment.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/backoffice")
 public class BackofficeController {
 
     @Autowired
-    PaymentService paymentService;
+    private PaymentService paymentService;
 
     @RequestMapping("/payments")
-    public String payment(Model model) {
-        List<Payment> payments = paymentService.getAllPayments(); // Assuming this method returns a List<Payment>
-        model.addAttribute("payments", payments);
+    public String payments(Model model,
+                           @RequestParam(defaultValue = "0") int page,
+                           @RequestParam(defaultValue = "10") int size) {
+        Page<Payment> paymentPage = paymentService.getAllPayments(page, size);
+        model.addAttribute("payments", paymentPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", paymentPage.getTotalPages());
         return "payments";
     }
-
 }
--- src/main/java/com/superfintech/superpayment/repository/PaymentRepository.java
+++ src/main/java/com/superfintech/superpayment/repository/PaymentRepository.java
@@ -2,9 +2,10 @@ package com.superfintech.superpayment.repository;
 
 import com.superfintech.superpayment.entity.Payment;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentRepository extends CrudRepository<Payment, String> {
+public interface PaymentRepository extends CrudRepository<Payment, String>, PagingAndSortingRepository<Payment, String> {
 
 }
